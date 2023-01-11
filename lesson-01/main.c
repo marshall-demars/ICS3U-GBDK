a7b275392f2f4ed80051ac97c056fa84871f879f
Marshall Demars <marshall.demars@mths.ca>
2023-01-11 16:04:27 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Marshall Demars
+//
+// Created by: Marshall Demars
+// Created on: Jan 2023
+// This program is the Space Aliens game for the Gameboy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+    printf("Hello, World!");
+}
